52c6ac780d5157a992dd771a554696804c440dc0
Juan Cruz Lopez Freytas <juancruzlopezfreytas@gmail.com>
2025-11-09 20:49:11 -0300

    BasePage and BaseTest added

---
+++ finalTaskATJava/src/test/java/test/BaseTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import driver.DriverSingleton;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = DriverSingleton.getDriver();
+    }
+
+    @AfterEach
+    public void stopBrowser() {
+        DriverSingleton.closeDriver();
+    }
+}
