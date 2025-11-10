51c4470154f80f72a69e05cd8c102b778545baff
Juan Cruz Lopez Freytas <juancruzlopezfreytas@gmail.com>
2025-11-10 00:45:32 -0300

    Null environment data handling added to TestDataReader

--- finalTaskATJava/src/test/java/service/TestDataReader.java
+++ finalTaskATJava/src/test/java/service/TestDataReader.java
@@ -3,7 +3,15 @@ package service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
+
+    private static String environment = System.getProperty("environment");
+    static {
+        if (environment == null) {
+            environment = "set1";
+        }
+    }
+
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(environment);
 
     public static String getTestData(String key){
         return resourceBundle.getString(key);
